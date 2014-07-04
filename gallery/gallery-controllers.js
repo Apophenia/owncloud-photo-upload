730294dcedaa741aecd3c547fd851d305da955b2
Apophenia <lyndsey@lyndseyjane.com>
2014-07-04 18:21:53 -0400

    Remove extraneous print lines

--- gallery/gallery-controllers.js
+++ gallery/gallery-controllers.js
@@ -4,10 +4,6 @@ galleryControllers.controller('listController',
 			      ["$scope", 
 			       'Gallery',
 			       function($scope, Gallery) {
-				   console.log("I know I'm not updating any bindings, but shouldn't this execute?");
-				   console.log(Gallery);
-				   // this is a rather contrived example copied from the Angular docs
-				   // Names must be longer than two characters to be valid
 				   var promise = Gallery.getPhotos();
 				   promise.then(function(result) {
 				       console.log(result);
