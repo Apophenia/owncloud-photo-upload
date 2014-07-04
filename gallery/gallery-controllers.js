fda2a62254b8f30af0c92ae3b02a59e911c6e910
Apophenia <lyndsey@lyndseyjane.com>
2014-07-04 12:16:20 -0400

    Temporarily implement pattern from documents to ensure async communication with factory works  Please enter the commit message for your changes. Lines starting

--- gallery/gallery-controllers.js
+++ gallery/gallery-controllers.js
@@ -4,9 +4,44 @@ galleryControllers.controller('listController',
 			      ["$scope", 
 			       'Gallery',
 			       function($scope, Gallery) {
-				   // ng-repeat won't iterate through 
-				   // file objects - working on this.
-				   $scope.photos = Gallery.getPhotos();
-			       }
-			      ]);
+				   console.log("I know I'm not updating any bindings, but shouldn't this execute?");
+				   // this is a rather contrived example copied from the Angular docs
+				   // Names must be longer than two characters to be valid
+				   var promise = Gallery.asyncGreet('R');
+				   promise.then(function(greeting) {
+				       console.log('Success: ' + greeting);
+				   }, function(reason) {
+				       console.log('Failed: ' + reason);
+				   }, function(update) {
+				       console.log('Got notification: ' + update);
+				   });
+			       }]);
+			       
+/*
+			       function($scope, Gallery) {
+				   var promise = Gallery.getPhotos();
+				   console.log(promise);
+				   promise.then(function(returnedPhotos) {
+				       console.log("Success!");
+				       console.log(returnedPhotos);
+				   }, function(reason) {
+				       console.log("Failed!");
+				       console.log(reason);
+				   }, function(update) {
+				       console.log("Got notification!");
+				       console.log(update);
+				   });
+			       }*/
+
+
+
+
+
+
+
+
+
+
+
+
 
--- gallery/gallery-service.js
+++ gallery/gallery-service.js
@@ -1,27 +1,54 @@
 var galleryServices = angular.module('galleryServices', []);
 
-
-galleryServices.factory('Gallery', function galleryFactory() {
+galleryServices.factory('Gallery', function galleryFactory($q) {
     var service = {};	
 
+    // This is example is copied from the angular docs
+    service.asyncGreet = function asyncGreet(name) {
+	var deferred = $q.defer();
+	
+	setTimeout(function() {
+	    deferred.notify('About to greet ' + name + '.');
+	    
+	    if (name.length > 2) {
+		deferred.resolve('Hello, ' + name + '!');
+	    } else {
+		deferred.reject('Greeting ' + name + ' is not allowed.');
+      }
+    }, 1000);
+
+    return deferred.promise;
+    };
+    
+    /*
     service.getPhotos = function() {
+	var deferred = $q.defer();
 	var photos = [];	
 	var files = navigator.getDeviceStorage('pictures');
 	var cursor = files.enumerate();
-
+	
 	cursor.onsuccess = function () {
 	    var file = this.result;
-	    photos.push(file);
+	    photos.push({name: file.name});
 	    if (!this.done) {
 		this.continue();
 	    }
+	    else {
+		if (photos.length > 0) {
+		    deferred.resolve(photos);	
+		}
+		else {
+		    deferred.reject("Unable to retrieve files from storage.");
+		}
+		console.log("returning the promise...");
+		return deferred.promise; 
+	    }
 	};
-
+	
 	cursor.onerror = function () {
 	    console.warn("File not found: " + this.error);
 	};
-	
-	return photos;	
-    };
+    }; */
+    
     return service;
 });
