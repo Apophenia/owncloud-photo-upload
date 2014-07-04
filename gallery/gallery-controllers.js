04cbd1fafa914f7627bc15d7a38f4df70b783571
Apophenia <lyndsey@lyndseyjane.com>
2014-07-04 18:18:23 -0400

    Updated promise code with relevant file system API calls  Please enter the commit message for your changes. Lines starting

--- gallery/gallery-controllers.js
+++ gallery/gallery-controllers.js
@@ -1,43 +1,22 @@
-var galleryControllers = angular.module("galleryControllers", []);
+var galleryControllers = angular.module("galleryControllers", ['galleryServices']);
 
 galleryControllers.controller('listController', 
 			      ["$scope", 
 			       'Gallery',
 			       function($scope, Gallery) {
 				   console.log("I know I'm not updating any bindings, but shouldn't this execute?");
+				   console.log(Gallery);
 				   // this is a rather contrived example copied from the Angular docs
 				   // Names must be longer than two characters to be valid
-				   var promise = Gallery.asyncGreet('R');
-				   promise.then(function(greeting) {
-				       console.log('Success: ' + greeting);
+				   var promise = Gallery.getPhotos();
+				   promise.then(function(result) {
+				       console.log(result);
 				   }, function(reason) {
 				       console.log('Failed: ' + reason);
 				   }, function(update) {
 				       console.log('Got notification: ' + update);
 				   });
 			       }]);
-			       
-/*
-			       function($scope, Gallery) {
-				   var promise = Gallery.getPhotos();
-				   console.log(promise);
-				   promise.then(function(returnedPhotos) {
-				       console.log("Success!");
-				       console.log(returnedPhotos);
-				   }, function(reason) {
-				       console.log("Failed!");
-				       console.log(reason);
-				   }, function(update) {
-				       console.log("Got notification!");
-				       console.log(update);
-				   });
-			       }*/
-
-
-
-
-
-
 
 
 
--- gallery/gallery-service.js
+++ gallery/gallery-service.js
@@ -1,54 +1,38 @@
 var galleryServices = angular.module('galleryServices', []);
 
-galleryServices.factory('Gallery', function galleryFactory($q) {
+galleryServices.factory('Gallery', function($q) {
     var service = {};	
 
     // This is example is copied from the angular docs
-    service.asyncGreet = function asyncGreet(name) {
-	var deferred = $q.defer();
-	
-	setTimeout(function() {
-	    deferred.notify('About to greet ' + name + '.');
-	    
-	    if (name.length > 2) {
-		deferred.resolve('Hello, ' + name + '!');
-	    } else {
-		deferred.reject('Greeting ' + name + ' is not allowed.');
-      }
-    }, 1000);
-
-    return deferred.promise;
-    };
-    
-    /*
     service.getPhotos = function() {
 	var deferred = $q.defer();
+	
 	var photos = [];	
 	var files = navigator.getDeviceStorage('pictures');
 	var cursor = files.enumerate();
 	
-	cursor.onsuccess = function () {
+	cursor.onsuccess = function() {
 	    var file = this.result;
-	    photos.push({name: file.name});
-	    if (!this.done) {
-		this.continue();
+	    console.log("File found: " + this.result.name);
+
+	    if (this.done && photos.length > 0) {
+		deferred.resolve(photos);
+		return deferred.promise;
+	    }
+	    else if (this.done) {
+		console.log("done");
+		deferred.reject("No photos found.");
+		return deferred.promise;
 	    }
 	    else {
-		if (photos.length > 0) {
-		    deferred.resolve(photos);	
-		}
-		else {
-		    deferred.reject("Unable to retrieve files from storage.");
-		}
-		console.log("returning the promise...");
-		return deferred.promise; 
+		this.continue();
 	    }
 	};
-	
-	cursor.onerror = function () {
-	    console.warn("File not found: " + this.error);
+
+	cursor.onerror = function() {
+	    deferred.reject("Error browsing the file system.");
+	    return deferred.promise;
 	};
-    }; */
-    
+    };
     return service;
 });
