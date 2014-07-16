989997b52814ac82871665591c4f6006fae81cda
Apophenia <lyndsey@lyndseyjane.com>
2014-07-15 22:48:54 -0400

    Adding webdav service to components folder

--- components/webdav-service.js
+++ components/webdav-service.js
@@ -1,11 +1,31 @@
 var webdavServices = angular.module('webdavServices', []);
 
 webdavServices.factory('webDAV', function($q) {
-    var factory = {};	
+    var factory = {};
 
-    factory.propfind = function() {
+    factory.genAuth = function() {
     };
 
+    factory.genPropRequestBody = function(properties) {
+	var body;
+	return body;
+    }
+
+    factory.propfind = function(url, properties) {
+	var deferred = $q.deferred();
+	var credentials = this.genAuth();
+	var xhr = new XMLHttpRequest({mozSystem: true});
+	xhr.open("PROPFIND", url);
+	xhr.setRequestHeader("Auth", credentials);
+	xhr.setRequestHeader("Content-type", "application/xml; charset='utf-8'");
+	if (!properties) {
+	    xhr.send();
+	    }
+	else {
+	    var body = genPropRequestBody(properties);
+	    xhr.send(body);
+	}
+	
     return factory;
 });
 
