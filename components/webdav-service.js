76206b7b96f0b34abcc82ff2aafd8d0434e9c931
Apophenia <lyndsey@lyndseyjane.com>
2014-07-25 09:20:21 -0400

    Return webdav-service factory object directly

--- components/webdav-service.js
+++ components/webdav-service.js
@@ -1,72 +1,72 @@
 var webdavServices = angular.module('webdavServices', []);
 
 webdavServices.factory('webDAV', function($q) {
-    var factory = {};
-    var serializer = new XMLSerializer();
+    return {
+	
+	genAuth: function() {
+	    // temporarily return auth for test:test until auth built
+	    return "Basic dGVzdDp0ZXN0";
+	},
 
-    factory.genAuth = function() {
-	// temporarily return auth for test:test until auth built
-	return "Basic dGVzdDp0ZXN0";
-    };
+	// returns an XML body including the given properties
+	genPropRequestBody: function(properties) {
+	    var serializer = new XMLSerializer();
+	    var propBody = document.implementation.createDocument("DAV:", "propfind");
+	    var prop = propBody.createElementNS("DAV:", "prop");
+	    for (var i = 0; i < properties.length; ++i) {
+		prop.appendChild(propBody.createElementNS("DAV:", properties[i]));
+	    }
+	    propBody.documentElement.appendChild(prop);
+	    return(serializer.serializeToString(propBody));
+	},
 
-    // returns an XML body including the given properties
-    factory.genPropRequestBody = function(properties) {
-	var propBody = document.implementation.createDocument("DAV:", "propfind");
-	var prop = propBody.createElementNS("DAV:", "prop");
-	for (var i = 0; i < properties.length; ++i) {
-	    prop.appendChild(propBody.createElementNS("DAV:", properties[i]));
-	}
-	propBody.documentElement.appendChild(prop);
-	return(serializer.serializeToString(propBody));
-    };
-
-    factory.propfind = function(url, properties) {
-	var deferred = $q.deferred();
-	var xhr = new XMLHttpRequest({mozSystem: true});
-	xhr.open("PROPFIND", url, true);
-	xhr.setRequestHeader("Auth", this.genAuth());
-	xhr.setRequestHeader("Content-type", "application/xml; charset='utf-8'");
-	xhr.onload = function (e) {
-	    if (xhr.readyState === 4) {
-		if (xhr.status === 200) {
-		    deferred.resolve(xhr.responseText);
-		} else {
-		    deferred.reject(xhr.statusText);
+	propfind: function(url, properties) {
+	    var deferred = $q.deferred();
+	    var xhr = new XMLHttpRequest({mozSystem: true});
+	    xhr.open("PROPFIND", url, true);
+	    xhr.setRequestHeader("Auth", this.genAuth());
+	    xhr.setRequestHeader("Content-type", "application/xml; charset='utf-8'");
+	    xhr.onload = function (e) {
+		if (xhr.readyState === 4) {
+		    if (xhr.status === 200) {
+			deferred.resolve(xhr.responseText);
+		    } else {
+			deferred.reject(xhr.statusText);
+		    }
 		}
+	    };
+	    xhr.onerror = function (e) {
+		deferred.reject(xhr.statusText);
+	    };
+	    if (!properties) {
+		xhr.send();
 	    }
-	};
-	xhr.onerror = function (e) {
-	    deferred.reject(xhr.statusText);
-	};
-	if (!properties) {
-	    xhr.send();
+	    else {
+		var body = genPropRequestBody(properties);
+		xhr.send(body);
 	    }
-	else {
-	    var body = genPropRequestBody(properties);
-	    xhr.send(body);
-	}
-	return deferred.promise;
-    };
+	    return deferred.promise;
+	},
 
-    factory.get = function(url) {
-	var deferred = $q.defer();
-	var xhr = new XMLHttpRequest({mozSystem: true});
-	xhr.open("GET", url, true);
-	xhr.setRequestHeader("Authorization", factory.genAuth());
-	xhr.onload = function (e) {
-	    if (xhr.readyState === 4) {
-		if (xhr.status === 200) {
-		    deferred.resolve(xhr.responseText);
-		} else {
-		    deferred.reject(xhr.statusText);
+	get: function(url) {
+	    var deferred = $q.defer();
+	    var xhr = new XMLHttpRequest({mozSystem: true});
+	    xhr.open("GET", url, true);
+	    xhr.setRequestHeader("Authorization", factory.genAuth());
+	    xhr.onload = function (e) {
+		if (xhr.readyState === 4) {
+		    if (xhr.status === 200) {
+			deferred.resolve(xhr.responseText);
+		    } else {
+			deferred.reject(xhr.statusText);
+		    }
 		}
-	    }
-	};
-	xhr.onerror = function (e) {
-	    deferred.reject(xhr.statusText);
-	};
-	xhr.send(null);
-	return deferred.promise;
+	    };
+	    xhr.onerror = function (e) {
+		deferred.reject(xhr.statusText);
+	    };
+	    xhr.send(null);
+	    return deferred.promise;
+	}
     };
-    return factory;
 });
