7cfa316336317dde9818abd590ce2801f438038f
Apophenia <lyndsey@lyndseyjane.com>
2014-07-25 09:05:07 -0400

    Return gallery-service factory object directly

--- gallery/gallery-service.js
+++ gallery/gallery-service.js
@@ -1,57 +1,54 @@
 var galleryServices = angular.module('galleryServices', []);
 
 galleryServices.factory('Gallery', ['$window', '$q', function($window, $q) {
-    var factory = {};	
+    return {		
+	getDeviceMedia: function() {
+	    var deferred = $q.defer();	
+	    var photos = [];	
+	    var files = $window.navigator.getDeviceStorage('pictures');
+	    var cursor = files.enumerate();
 
-    factory.getDeviceMedia = function() {
-	var deferred = $q.defer();	
-	var photos = [];	
-	var files = $window.navigator.getDeviceStorage('pictures');
-	var cursor = files.enumerate();
+	    cursor.onsuccess = function() {
+		if (this.done) {
+		    photos.forEach(function(photo) {
+			photo.src = $window.URL.createObjectURL(photo);
+		    });
+		    deferred.resolve(photos);
+		}
+		else {
+		    photos.push(this.result);
+		    this.continue();
+		}
+	    };
 
-	cursor.onsuccess = function() {
-	    if (this.done) {
-		photos.forEach(function(photo) {
-		    photo.src = $window.URL.createObjectURL(photo);
-		});
-		deferred.resolve(photos);
+	    cursor.onerror = function() {
+		deferred.reject("Error browsing the file system.");
+	    };
+
+	    return deferred.promise;
+	},
+	// this needs a refactor! perhaps with Underscore?
+	getMediaList: function(doc) {
+	    var result = [];
+	    var strippedArray = [];
+	    var links = doc.links;
+	    var imagePattern = new RegExp(".*(png|gif|jpg|jpeg|bmp)");
+	    for (var i=0; i<links.length; i++) {
+		match = links[i].getAttribute("href").match(imagePattern);
+		// strip to filename before pushing to array
+		if (match) { // probably unsafe
+		    result.push(match[0].replace(/^.*(\\|\/|\:)/, ''));
+		}
 	    }
-	    else {
-		photos.push(this.result);
-		this.continue();
-	    }
-	};
-
-	cursor.onerror = function() {
-	    deferred.reject("Error browsing the file system.");
-	};
-
-	return deferred.promise;
-    };
-
-    // this needs a refactor! perhaps with Underscore?
-    factory.getMediaList = function(doc) {
-	var result = [];
-	var strippedArray = [];
-	var links = doc.links;
-	var imagePattern = new RegExp(".*(png|gif|jpg|jpeg|bmp)");
-	for (var i=0; i<links.length; i++) {
-	    match = links[i].getAttribute("href").match(imagePattern);
-	    // strip to filename before pushing to array
-	    if (match) { // probably unsafe
-		result.push(match[0].replace(/^.*(\\|\/|\:)/, ''));
-	    }
-	}
-	strippedArray = result.filter(function(elem, pos) {
-	    return result.indexOf(elem) == pos;
-	});
-	return strippedArray;
-    };
-
-    factory.findNewElements = function (newArray, oldArray) {
-	return newArray.filter(function(x) {
-	    return (oldArray.indexOf(x) < 0);
+	    strippedArray = result.filter(function(elem, pos) {
+		return result.indexOf(elem) == pos;
 	    });
+	    return strippedArray;
+	},
+	findNewElements: function (newArray, oldArray) {
+	    return newArray.filter(function(x) {
+		return (oldArray.indexOf(x) < 0);
+	    });
+	}
     };
-    return factory;
 }]);
