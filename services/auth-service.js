3427ec6d1fbe97e1ac574ee51bf53083b020a9ab
Apophenia <lyndsey@lyndseyjane.com>
2014-08-23 13:41:09 -0400

    Removes traces of old API from auth service

--- services/auth-service.js
+++ services/auth-service.js
@@ -18,9 +18,6 @@ angular.module('uploadApp')
 		db = request.result;
 		var store = db.createObjectStore("auth", {keyPath: "id"});
 		console.log("successfully created objectStore");
-		
-		// Populate with initial data.
-		store.put({username: "test", password: "test", location: "http://demo.owncloud.org", id: 1});
 	    };
 
 	    request.onsuccess = function(event) {
@@ -33,18 +30,16 @@ angular.module('uploadApp')
 	    return deferred.promise;
 	}
 	
-	function storeAuth() {
+	function storeAuth(credentials) {
 	    var deferred = $q.defer();
 	    if(db === null){
 		deferred.reject("IndexedDB is not currently open.");
 	    }
 	    var tx = db.transaction("auth", "readwrite");
 	    var store = tx.objectStore("auth");
-	    var request = store.put({username: "test2", password: "test2", location: "http://demo.owncloud.org", id: 2});
+	    var request = store.put(credentials);
 	    request.onsuccess = function(e) {
-		deferred.resolve(true);
-	    };
-	    request.onerror = function() {
+		deferred.resolve(true); {
 		deferred.reject(request.error);
 	    };
 	    tx.onabort = function() {
@@ -63,7 +58,7 @@ angular.module('uploadApp')
 		var cursor = request.result;
 		var authObjs = [];
 		if (cursor) {
-		    // This returns a cursor result; we will want the "value"
+		    //Returns a full cursor result; we will want the "value"
 		    authObjs.push(this.result);
 		    deferred.resolve(authObjs);
 		}
@@ -73,10 +68,6 @@ angular.module('uploadApp')
 	    };
 	    return deferred.promise;
 	}
-
-	var username = "";
-	var password = "";
-	var installLocation = "";
     
     function encodeBasic(auth) {
 	return ("Basic " + $window.btoa(username+":"+password));
