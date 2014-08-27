ef418eebe7d3232234fd0639e1bfbe0ccf9cd648
Alessandro Cosentino <cosenal@gmail.com>
2014-08-27 18:13:28 +0200

    it shows the pictaaaaa

--- services/activity-service.js
+++ services/activity-service.js
@@ -10,7 +10,22 @@ angular.module('uploadApp')
 
             if (option.name === "share") {
                 filePath = option.data.filepaths[0];
-                deferred.resolve(filePath);
+
+                var storage = $window.navigator.getDeviceStorage('pictures');
+                var getRequest = storage.get(filePath);
+
+                //not sure about this, maybe two promises need to be piped
+                getRequest.onsuccess = function() {
+                    deferred.resolve($window.URL.createObjectURL(getRequest.result));
+                };
+
+                getRequest.onerror = function() {
+                    deferred.resolve("errorrr");
+                };
+
+                // TODO: handle error part
+                // getRequest.onerror = function() {
+                // }
             }
         });
 
