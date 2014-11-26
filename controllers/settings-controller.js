1322bfc5ae44b2ac95a7c39352ab1c2869a65916
Alessandro Cosentino <cosenal@gmail.com>
2014-11-26 14:18:58 -0500

    better indentation

--- controllers/settings-controller.js
+++ controllers/settings-controller.js
@@ -1,25 +1,30 @@
 angular.module("uploadApp")
-  .controller("settingsController", function ($window, $scope, $log, $q, Auth) {
-      $scope.$log = $log;
-      $scope.save = function() {
-	  $scope.updateStatus = "Updating...";
-	  Auth.store($scope.credentials).then(
-	      function(value) {
-		  $scope.updateStatus = "Updated";
-	      }, function () {
-		  $scope.updateStatus = "An error occured when updating account information."
-	      });
-      };
-	  
-	  $scope.populate = function() {
-	  Auth.init().then(function() {
-	      Auth.retrieve().then(
-		  function(credentials) {
-		      $scope.credentials = credentials;
-		  }, function(error) {
-		      $log.error(error);
-		  });
-	  });
-      };
-	  $scope.populate();
-      });
+.controller("settingsController", function ($window, $scope, $log, $q, Auth) {
+    $scope.$log = $log;
+    
+    $scope.save = function() {
+        $scope.updateStatus = "Updating...";
+        Auth.store($scope.credentials).then(
+            function(value) {
+                $scope.updateStatus = "Updated";
+            }, function () {
+            $scope.updateStatus = "An error occured when updating account 
+                information."
+            }
+        );
+    };
+
+    $scope.populate = function() {
+        Auth.init().then(function() {
+            Auth.retrieve().then(
+                function(credentials) {
+                    $scope.credentials = credentials;
+                }, function(error) {
+                    $log.error(error);
+                }
+            );
+        });
+    };
+
+    $scope.populate();
+});
