0199e217bba5467fdb06c5ab0244c70020234a59
Alessandro Cosentino <cosenal@gmail.com>
2014-11-26 01:17:19 -0500

    restyle of settings page

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ angular.module("uploadApp", ["ngRoute"])
         templateUrl: "partials/gallery.html",
         controller: "galleryController"
       })
-      .when("/options", {
-        templateUrl: "partials/options.html",
+      .when("/settings", {
+        templateUrl: "partials/settings.html",
 	      controller: "optionsController"
       })
       .when("/activity", {
